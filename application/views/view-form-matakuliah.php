477d11528adf26416347cd4cb79a8dd199801565
Reynaldy <renalfrontend@gmail.com>
2020-12-02 15:14:20 +0700

    Prepare first project and add file CI

---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    
+    <form action="<?= $this->config->base_url() ?>cetak" method='post'>
+        <label for="kode">Kode MTK : </label>
+        <input type="text" id='kode'><br>
+        <label for="nama">Nama MTK : </label>
+        <input type="text" id='nama'><br>
+
+        <select name="sks" id="sks">
+            <option value=" ">Pilih SKS</option>
+            <option value="2">2</option>
+            <option value="2">3</option>
+            <option value="2">4</option>
+        </select><br>
+
+        <button type="submit">Submit</button>
+    </form>
+
+</body>
+</html>
