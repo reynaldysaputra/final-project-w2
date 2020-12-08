f70dd5c148c51a2cfe95956248729838d2169bbc
Renaldy <renalfrontend@gmail.com>
2020-12-09 00:21:38 +0700

    Membuat halaman gejala 100% complete dan halaman tentang yang baru 25% progress by:renaldy

---
+++ application/views/pages/tentang.php
@@ -0,0 +1,16 @@
+    <!-- Display Container -->
+    <div class="jumbotron jumbotron-fluid display-container" id='containerGejala'>
+        <div class="container">
+            <div class="row">
+                <div class="col xl5 l5 s12">
+                    <p class='p-1'>Tentang</p>
+                    <h1 class='h-1'>Tentang Kami</h1>
+                    <p id='contentHeader'>Masing-masing orang memiliki respons yang berbeda terhadap COVID-19. Sebagian besar orang yang terpapar virus ini akan mengalami gejala ringan hingga sedang, dan akan pulih tanpa perlu dirawat di rumah sakit. Orang dengan gejala ringan yang dinyatakan sehat harus melakukan perawatan mandiri di rumah. Rata-rata gejala akan muncul 5–6 hari setelah seseorang pertama kali terinfeksi virus ini, tetapi bisa juga 14 hari setelah terinfeksi.</p>
+                </div>
+                <div class="col xl7 l7 s12 img-header">
+                    <img src="<?= $this->config->base_url()?>assets/img/tentang/develop.png" alt="header" class='img-1' id='gambarTeam'>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Akhir Display Container -->
