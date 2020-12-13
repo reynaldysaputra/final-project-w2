e48f2edb4ff682ab71d91ed754c6a3a0d7256c2c
verdianto <verdianto.vd@gmail.com>
2020-12-13 14:58:22 +0700

    Menambahkan Model & Form Validation,Setting Autoload & Database fitur Input Feedback

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/application/';
+$config['base_url'] = 'http://localhost/Tugas/final-project-w2/';
 
 /*
 |--------------------------------------------------------------------------
